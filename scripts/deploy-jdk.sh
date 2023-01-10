a190de32c6410e6aba4dcad7589f67a99821925f
wangenyong <wangenyong@gmail.com>
2023-01-10 15:06:21 +0800

    submit

--- scripts/deploy-jdk.sh
+++ scripts/deploy-jdk.sh
@@ -24,11 +24,9 @@ else
         cd /opt/marmot/jdk1.8.0_*
         JDK_PATH="JAVA_HOME="`pwd`
 
-        echo '***** JAVA_HOME *****'
+        echo '#***** JAVA_HOME *****' >> $MARMOT_PROFILE
         echo "export "$JDK_PATH >> $MARMOT_PROFILE
         echo 'export PATH=$PATH:$JAVA_HOME/bin' >> $MARMOT_PROFILE
-
-        source "/etc/profile"
 
         echo "jdk profile setting success!"
     fi
