a3c40d801bc691482d2b673f210884b72936464b
wangenyong <wangenyong@gamil.com>
2023-01-16 11:27:40 +0800

    submity

--- scripts/auto-ssh-config.sh
+++ scripts/auto-ssh-config.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+source ./log.sh
+
 # 修改SSH配置文件
 SSH_CONFIG_FILE="/etc/ssh/ssh_config"
 
-if [ $(grep -c "StrictHostKeyChecking no" $SSH_CONFIG_FILE) -eq '0' ]; then
+log_info "开始配置SSH免密登录"
+
+if [ `grep -c "StrictHostKeyChecking no" $SSH_CONFIG_FILE` -eq '0' ]; then
     sed -i '/StrictHostKeyChecking/s/^#//; /StrictHostKeyChecking/s/ask/no/' $SSH_CONFIG_FILE
 fi
 
@@ -16,6 +20,6 @@ fi
 IFS=$'\n' read -d '' -r -a lines <../conf/workers
 
 for worker in ${lines[@]}; do
-    echo root@$worker
     sshpass -f ../conf/ssh_passwd ssh-copy-id root@$worker
+    log_info "root@$worker SSH免密配置完成"
 done
---
+++ scripts/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source ./log.sh
+
+log_info "log info"
