d338118ab7f0076757844734ff36b314c56c5a3a
wangenyong <wangenyong@gamil.com>
2023-01-31 09:37:33 +0800

    config hdfs-site.xml

--- scripts/config-hadoop.sh
+++ scripts/config-hadoop.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-
 SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 HOME_DIR="$(dirname $SCRIPT_DIR)"
-
 
 NAME_NODE_CONFIG='
     <!-- 指定NameNode的地址 -->\
@@ -12,18 +10,12 @@ NAME_NODE_CONFIG='
         <value>hdfs://'$(head -n 1 $HOME_DIR/conf/workers)':8020</value>\
     </property>'
 
-sed -in '/<\/configuration>/i\'"$NAME_NODE_CONFIG" $HADOOP_HOME/etc/hadoop/core-site.xml
-
-
 DATA_DIR_CONFIG='
     <!-- 指定hadoop数据的存储目录 -->\
     <property>\
         <name>hadoop.tmp.dir</name>\
         <value>'$HADOOP_HOME'/data</value>\
     </property>'
-
-sed -in '/<\/configuration>/i\'"$DATA_DIR_CONFIG" $HADOOP_HOME/etc/hadoop/core-site.xml
-
 
 HDFS_USER_CONFIG='
     <!-- 配置HDFS网页登录使用的静态用户为marmot -->\
@@ -32,4 +24,23 @@ HDFS_USER_CONFIG='
         <value>marmot</value>\
     </property>'
 
+sed -in '/<\/configuration>/i\'"$NAME_NODE_CONFIG" $HADOOP_HOME/etc/hadoop/core-site.xml
+sed -in '/<\/configuration>/i\'"$DATA_DIR_CONFIG" $HADOOP_HOME/etc/hadoop/core-site.xml
 sed -in '/<\/configuration>/i\'"$HDFS_USER_CONFIG" $HADOOP_HOME/etc/hadoop/core-site.xml
+
+WEB_CONFIG='
+    <!-- nn web端访问地址-->\
+	<property>\
+        <name>dfs.namenode.http-address</name>\
+        <value>'$(head -n 1 $HOME_DIR/conf/workers)':9870</value>\
+    </property>'
+
+SECONDARY_WEB_CONFIG='
+    <!-- 2nn web端访问地址-->\
+    <property>\
+        <name>dfs.namenode.secondary.http-address</name>\
+        <value>'$(sed -n '3p' $HOME_DIR/conf/workers)':9868</value>\
+    </property>'
+
+sed -in '/<\/configuration>/i\'"$WEB_CONFIG" $HADOOP_HOME/etc/hadoop/hdfs-site.xml
+sed -in '/<\/configuration>/i\'"$SECONDARY_WEB_CONFIG" $HADOOP_HOME/etc/hadoop/hdfs-site.xml
