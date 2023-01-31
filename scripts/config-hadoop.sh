6a316bce1b6d47f1f0024a69a75cf1147589a795
wangenyong <wangenyong@gamil.com>
2023-01-31 15:53:42 +0800

    submit

--- scripts/config-hadoop.sh
+++ scripts/config-hadoop.sh
@@ -95,12 +95,36 @@ ENV_CONFIG='
         <value>JAVA_HOME,HADOOP_COMMON_HOME,HADOOP_HDFS_HOME,HADOOP_CONF_DIR,CLASSPATH_PREPEND_DISTCACHE,HADOOP_YARN_HOME,HADOOP_MAPRED_HOME</value>\
     </property>'
 
+LOG_AGGREGATION_CONFIG='
+    <!-- 开启日志聚集功能 -->\
+    <property>\
+        <name>yarn.log-aggregation-enable</name>\
+        <value>true</value>\
+    </property>'
+
+LOG_SERVER_CONFIG='
+    <!-- 设置日志聚集服务器地址 -->\
+    <property>\
+        <name>yarn.log.server.url</name>\  
+        <value>'$(head -n 1 $HOME_DIR/conf/workers)':19888/jobhistory/logs</value>\
+    </property>'
+
+LOG_RETAIN_CONFIG='
+    <!-- 设置日志保留时间为7天 -->\
+    <property>\
+        <name>yarn.log-aggregation.retain-seconds</name>\
+        <value>604800</value>\
+    </property>'
+
 YARN_SITE_FILE=$HADOOP_HOME/etc/hadoop/yarn-site.xml
 # 判断 yarn-site.xml 文件是否已经配置
 if [ $(grep -c "yarn.nodemanager.aux-services" $YARN_SITE_FILE) -eq '0' ]; then
     sed -in '/<\/configuration>/i\'"$MR_CONFIG" $YARN_SITE_FILE
     sed -in '/<\/configuration>/i\'"$RM_HOSTNAME" $YARN_SITE_FILE
     sed -in '/<\/configuration>/i\'"$ENV_CONFIG" $YARN_SITE_FILE
+    sed -in '/<\/configuration>/i\'"$LOG_AGGREGATION_CONFIG" $YARN_SITE_FILE
+    sed -in '/<\/configuration>/i\'"$LOG_SERVER_CONFIG" $YARN_SITE_FILE
+    sed -in '/<\/configuration>/i\'"$LOG_RETAIN_CONFIG" $YARN_SITE_FILE
     log_info "yarn-site.xml 文件配置完成！"
 else
     log_warn "yarn-site.xml 文件已配置！"
@@ -116,10 +140,26 @@ MR_YARN_CONFIG='
         <value>yarn</value>\
     </property>'
 
+HISTORY_ADDRESS_CONFIG='
+    <!-- 历史服务器端地址 -->\
+    <property>\
+        <name>mapreduce.jobhistory.address</name>\
+        <value>'$(head -n 1 $HOME_DIR/conf/workers)':10020</value>\
+    </property>'
+
+HISTORY_WEB_CONFIG='
+    <!-- 历史服务器 Web 端地址 -->\
+    <property>\
+        <name>mapreduce.jobhistory.webapp.address</name>\
+        <value>'$(head -n 1 $HOME_DIR/conf/workers)':19888</value>\
+    </property>'
+
 MR_SITE_FILE=$HADOOP_HOME/etc/hadoop/mapred-site.xml
 # 判断 mapred-site.xml 文件是否已经配置
 if [ $(grep -c "mapreduce.framework.name" $MR_SITE_FILE) -eq '0' ]; then
     sed -in '/<\/configuration>/i\'"$MR_YARN_CONFIG" $MR_SITE_FILE
+    sed -in '/<\/configuration>/i\'"$HISTORY_ADDRESS_CONFIG" $MR_SITE_FILE
+    sed -in '/<\/configuration>/i\'"$HISTORY_WEB_CONFIG" $MR_SITE_FILE
     log_info "mapred-site.xml 文件配置完成！"
 else
     log_warn "mapred-site.xml 文件已配置！"
@@ -128,4 +168,4 @@ fi
 #
 # 配置 workers
 #
-cat $HOME_DIR/conf/workers >>$HADOOP_HOME/etc/hadoop/workers
+cat $HOME_DIR/conf/workers >$HADOOP_HOME/etc/hadoop/workers
