dbf0e3327d258a8bb09214b6a830a5d91c8a7a73
wangenyong <wangenyong@gamil.com>
2023-02-03 11:29:42 +0800

    deploy hive

--- scripts/deploy-hive.sh
+++ scripts/deploy-hive.sh
@@ -38,10 +38,6 @@ else
         source /etc/profile
 
         log_info "HIVE_HOME 环境变量设置完成: "$HIVE_HOME
-
-        # 解决日志冲突问题
-        mv $HIVE_HOME/lib/log4j-slf4j-impl-2.10.0.jar $HIVE_HOME/lib/log4j-slf4j-impl-2.10.0.jar.bak
-
     else
         log_warn "HIVE_HOME 环境变量已配置"
     fi
@@ -63,13 +59,15 @@ HIVE_SITE_FILE=$HIVE_HOME/conf/hive-site.xml
 if [ ! -f $HIVE_SITE_FILE ]; then
     # 拷贝 hive-site.xml 模板文件
     cp $HOME_DIR/template/configuration.xml $HIVE_SITE_FILE
+    chmod 755 $HIVE_SITE_FILE
     # 拷贝 mysql 驱动
     cp $HOME_DIR/softwares/mysql/mysql-connector-java-5.1.27-bin.jar $HIVE_HOME/lib/
+    # 解决日志冲突问题
+    mv $HIVE_HOME/lib/log4j-slf4j-impl-2.10.0.jar $HIVE_HOME/lib/log4j-slf4j-impl-2.10.0.jar.bak
     # 设置拷贝文件的权限
     chown marmot:marmot -R $HIVE_HOME
 
-
-    MYSQL_URL_CONFIG='  
+    MYSQL_URL_CONFIG='
     <property>\
         <name>javax.jdo.option.ConnectionURL</name>\
         <value>jdbc:mysql://'$(head -n 1 $HOME_DIR/conf/workers)':3306/metastore?useSSL=false</value>\
@@ -90,12 +88,80 @@ if [ ! -f $HIVE_SITE_FILE ]; then
     MYSQL_PASSWD_CONFIG='
     <property>\
         <name>javax.jdo.option.ConnectionPassword</name>\
-        <value>Qs23=zs32</value>\
+        <value>yee-ha7X</value>\
     </property>'
 
+    HIVE_WAREHOUSE_CONFIG='
+    <property>\
+        <name>hive.metastore.warehouse.dir</name>\
+        <value>/user/hive/warehouse</value>\
+    </property>'
 
-    
+    HIVE_VERIFICATION_CONFIG='
+    <property>\
+        <name>hive.metastore.schema.verification</name>\
+        <value>false</value>\
+    </property>'
+
+    HIVE_THRIFT_PORT='
+    <property>\
+        <name>hive.server2.thrift.port</name>\
+        <value>10000</value>\
+    </property>'
+
+    HIVE_THRIFT_HOST='
+    <property>\
+        <name>hive.server2.thrift.bind.host</name>\
+        <value>'$(head -n 1 $HOME_DIR/conf/workers)'</value>\
+    </property>'
+
+    HIVE_API_AUTH='
+    <property>\
+        <name>hive.metastore.event.db.notification.api.auth</name>\
+        <value>false</value>\
+    </property>'
+
+    HIVE_CLI_HEADER='
+    <property>\
+        <name>hive.cli.print.header</name>\
+        <value>true</value>\
+    </property>'
+
+    HIVE_CLI_DB='
+    <property>\
+        <name>hive.cli.print.current.db</name>\
+        <value>true</value>\
+    </property>'
+
+    HIVE_METASTORE_URIS='
+    <!-- 指定存储元数据要连接的地址 -->
+    <property>
+        <name>hive.metastore.uris</name>
+        <value>thrift://'$(head -n 1 $HOME_DIR/conf/workers)':9083</value>
+    </property>'
+
     sed -in '/<\/configuration>/i\'"$MYSQL_URL_CONFIG" $HIVE_SITE_FILE
-
+    sed -in '/<\/configuration>/i\'"$MYSQL_DRIVER_NAME_CONFIG" $HIVE_SITE_FILE
+    sed -in '/<\/configuration>/i\'"$MYSQL_USERNAME_CONFIG" $HIVE_SITE_FILE
+    sed -in '/<\/configuration>/i\'"$MYSQL_PASSWD_CONFIG" $HIVE_SITE_FILE
+    sed -in '/<\/configuration>/i\'"$HIVE_WAREHOUSE_CONFIG" $HIVE_SITE_FILE
+    sed -in '/<\/configuration>/i\'"$HIVE_VERIFICATION_CONFIG" $HIVE_SITE_FILE
+    sed -in '/<\/configuration>/i\'"$HIVE_THRIFT_PORT" $HIVE_SITE_FILE
+    sed -in '/<\/configuration>/i\'"$HIVE_THRIFT_HOST" $HIVE_SITE_FILE
+    sed -in '/<\/configuration>/i\'"$HIVE_API_AUTH" $HIVE_SITE_FILE
+    sed -in '/<\/configuration>/i\'"$HIVE_CLI_HEADER" $HIVE_SITE_FILE
+    sed -in '/<\/configuration>/i\'"$HIVE_CLI_DB" $HIVE_SITE_FILE
+    sed -in '/<\/configuration>/i\'"$HIVE_METASTORE_URIS" $HIVE_SITE_FILE
 
 fi
+
+# 判断元数据库是否存在
+mysql -uroot -pyee-ha7X -e "use metastore"
+
+if [[ $? -ne 0 ]]; then
+    log_info "创建元数据库 metastore"
+    mysql -uroot -pyee-ha7X -e "create database metastore"
+    schematool -initSchema -dbType mysql -verbose
+else
+    log_info "元数据库 metastore 已创建"
+fi 
