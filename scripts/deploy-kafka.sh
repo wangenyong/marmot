dfb5a2d2547f390f9322965b71ff5026c0b1a4eb
wangenyong <wangenyong@gamil.com>
2023-02-09 17:17:25 +0800

    submit

--- scripts/deploy-kafka.sh
+++ scripts/deploy-kafka.sh
@@ -40,7 +40,7 @@ else
         KAFKA_HOME="KAFKA_HOME="$(pwd)
         cd -
 
-        echo -e >>$KAFKA_HOME
+        echo -e >>$MARMOT_PROFILE
         echo '#***** KAFKA_HOME *****' >>$MARMOT_PROFILE
         echo "export "$KAFKA_HOME >>$MARMOT_PROFILE
         echo 'export PATH=$PATH:$KAFKA_HOME/bin' >>$MARMOT_PROFILE
@@ -52,4 +52,45 @@ else
         log_warn "KAFKA_HOME 环境变量已配置"
     fi
     log_info "========== KAFKA 安装完成 =========="
+fi
+
+#################################
+# 配置集群节点
+#################################
+source /etc/profile
+
+
+
+if [ ! -d "$KAFKA_HOME/logs" ]; then
+    mkdir -p $KAFKA_HOME/logs
+
+    i=1
+    connect=""
+    length=${#kafka_nodes[@]}
+    for node in ${kafka_nodes[@]}; do
+        if [ $i -eq $length ]; then
+            connect="$connect$node:2181/kafka"
+        else
+            connect="$connect$node:2181,"
+        fi
+        let i+=1
+    done
+    cp $KAFKA_HOME/config/server.properties $KAFKA_HOME/config/server.properties.bak
+    sed -i -r '/^log.dirs/s|.*|log.dirs='$KAFKA_HOME'/logs|' $KAFKA_HOME/config/server.properties
+    sed -i -r '/^zookeeper.connect=/s|.*|zookeeper.connect='$connect'|' $KAFKA_HOME/config/server.properties
+
+    # 修改 kafka 项目权限为 marmot:marmot
+    chown marmot:marmot -R $KAFKA_HOME
+
+    # 集群分发 kafka
+    sh $SCRIPT_DIR/msync.sh $ZOOKEEPER_NODES $KAFKA_HOME
+    # 集群分发环境变量
+    sh $SCRIPT_DIR/msync.sh $KAFKA_NODES /etc/profile.d/marmot_env.sh
+
+    i=1
+    for node in ${kafka_nodes[@]}; do
+        cmd="sed -i -r '/^broker.id=/s|.*|broker.id='$i'|' $KAFKA_HOME/config/server.properties"
+        ssh $HADOOP_USER@$node $cmd
+        let i+=1
+    done
 fi
