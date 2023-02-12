ded413627a984a3d5da5cedf06391c94769702fa
wangenyong <wangenyong@gamil.com>
2023-02-12 21:30:43 +0800

    submit

--- scripts/deploy-vsftp.sh
+++ scripts/deploy-vsftp.sh
@@ -1,42 +1,52 @@
 #!/bin/bash
 
-#################################
+#############################################################################################
 #
 # vsftp version "3.0.2"
 #
-# 配置 vsftp
+# configure vsftp
 #
-#################################
+#############################################################################################
 
-# 获取当前脚本所在目录和项目根目录
+# get script directory and home directory
 SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 HOME_DIR="$(dirname $SCRIPT_DIR)"
-
-# 加载日志打印脚本
-source $SCRIPT_DIR/log.sh
-# 加载配置文件
+# loading config file
 source $HOME_DIR/conf/config.conf
+# loading printf file
+source $HOME_DIR/conf/printf.conf
 
 TAG=".*"
 PKG="vsftpd"
-
 package_name=$(rpm -qa | grep "^${PKG}${TAG}")
-if [ $? -eq 0 ]; then
-    log_warn "${package_name} 已存在，无需安装"
-else
-    log_info "========== 开始安装配置 VSFTP =========="
-    rpm -ivh $HOME_DIR/softwares/packages/vsftpd-3.0.2-28.el7.x86_64.rpm
 
-    systemctl enable vsftpd.service
-    systemctl start vsftpd.service
+printf -- "${INFO}========== INSTALL VSFTP ==========${END}\n"
+if [ $? -eq 0 ]; then
+    printf -- "${SUCCESS}========== VSFTP INSTALLED ==========${END}\n"
+    printf -- "\n"
+    exit 0
 fi
+
+#############################################################################################
+# install vsftp
+#############################################################################################
+printf -- "${INFO}>>> Install vsftp.${END}\n"
+
+rpm -ivh $HOME_DIR/softwares/packages/vsftpd-3.0.2-28.el7.x86_64.rpm
+systemctl enable vsftpd.service
+systemctl start vsftpd.service
+
+#############################################################################################
+# configure vsftp
+#############################################################################################
+printf -- "\n"
+printf -- "${INFO}>>> Configure vsftp.${END}\n"
 
 VSFTP_CONF=/etc/vsftpd/vsftpd.conf
 USER_LIST=/etc/vsftpd/user_list
 CHROOT_LIST=/etc/vsftpd/chroot_list
 
 if [ $(grep -c "anonymous_enable=YES" $VSFTP_CONF) -ne '0' ]; then
-    log_info "修改 vsftp 配置文件"
     cp $VSFTP_CONF $VSFTP_CONF.bak
     sed -i -r '/^anonymous_enable/s/YES/NO/g' $VSFTP_CONF
     sed -i -r '/^#chown_uploads/s/^#//; /^chown_uploads/s/YES/NO/' $VSFTP_CONF
@@ -52,12 +62,15 @@ if [ $(grep -c "anonymous_enable=YES" $VSFTP_CONF) -ne '0' ]; then
 
     if [ ! -f $CHROOT_LIST ]; then
         touch $CHROOT_LIST && echo $HADOOP_USER >>$CHROOT_LIST
-    fi
-    
+    fi  
     systemctl restart vsftpd.service
+
+    printf -- "${SUCCESS}Configure vsftp configure successful.${END}\n"
 else
-    log_info "vsftp 配置文件已修改"
+    printf -- "${SUCCESS}Vsftp configurtion is complete.${END}\n"
 fi
 
+printf -- "\n"
+printf -- "${SUCCESS}========== VSFTP INSTALL SUCCESSFUL ==========${END}\n"
 
 
