c6ae1f9eb6b7d7cb0c45e926223988c00b6b6341
wangenyong <wangenyong@gamil.com>
2023-02-12 20:58:33 +0800

    submit

--- scripts/deploy-azkaban.sh
+++ scripts/deploy-azkaban.sh
@@ -1,68 +1,71 @@
 #!/bin/bash
 
-#################################
+#############################################################################################
 #
 # azkaban version "3.84.4"
 #
-# 配置 azkaban
+# configure azkaban
 #
-#################################
+#############################################################################################
 
-# 获取当前脚本所在目录和项目根目录
+# get script directory and home directory
 SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 HOME_DIR="$(dirname $SCRIPT_DIR)"
-
-# 加载日志打印脚本
-source $SCRIPT_DIR/log.sh
-# 加载配置文件
+# loading config file
 source $HOME_DIR/conf/config.conf
-
+# loading printf file
+source $HOME_DIR/conf/printf.conf
+# loading azkaban nodes
 IFS=',' read -ra azkaban_nodes <<<$AZKABAN_NODES
 
-log_info "========== 开始配置 AZKABAN =========="
-
 AZKABAN_HOME=/opt/marmot/azkaban
-# 判断 hadoop 是否已经安装
+
+printf -- "${INFO}========== INSTALL AZKABAN ==========${END}\n"
 if [ -d $AZKABAN_HOME ]; then
-    log_warn "azkaban 已经安装!"
-else
-    mkdir -p $AZKABAN_HOME
-    # 安装 azkaban
-    pv $HOME_DIR/softwares/azkaban/azkaban-db-3.84.4.tar.gz | tar -zx -C $AZKABAN_HOME
-    pv $HOME_DIR/softwares/azkaban/azkaban-exec-server-3.84.4.tar.gz | tar -zx -C $AZKABAN_HOME
-    pv $HOME_DIR/softwares/azkaban/azkaban-web-server-3.84.4.tar.gz | tar -zx -C $AZKABAN_HOME
-    mv $AZKABAN_HOME/azkaban-exec-server-3.84.4 $AZKABAN_HOME/azkaban-exec
-    mv $AZKABAN_HOME/azkaban-web-server-3.84.4 $AZKABAN_HOME/azkaban-web
-
-    # 创建环境变量文件
-    MARMOT_PROFILE="/etc/profile.d/marmot_env.sh"
-    if [ ! -f $MARMOT_PROFILE ]; then
-        touch $MARMOT_PROFILE
-    fi
-
-    # 配置 azkaban 环境变量
-    if [ $(grep -c "AZKABAN_HOME" $MARMOT_PROFILE) -eq '0' ]; then
-        echo -e >>$MARMOT_PROFILE
-        echo '#***** AZKABAN_HOME *****' >>$MARMOT_PROFILE
-        echo "export AZKABAN_HOME="$AZKABAN_HOME >>$MARMOT_PROFILE
-
-        source /etc/profile
-
-        log_info "AZKABAN_HOME 环境变量设置完成: "$AZKABAN_HOME
-    else
-        log_warn "AZKABAN_HOME 环境变量已配置"
-    fi
-
-    chown marmot:marmot -R $AZKABAN_HOME
+    printf -- "${SUCCESS}========== AZKABAN INSTALLED ==========${END}\n"
+    printf -- "\n"
+    exit 0
 fi
 
-#################################
-# 配置 mysql
-#################################
+#############################################################################################
+# install azkaban
+#############################################################################################
+printf -- "${INFO}>>> Install azkaban.${END}\n"
+mkdir -p $AZKABAN_HOME
+pv $HOME_DIR/softwares/azkaban/azkaban-db-3.84.4.tar.gz | tar -zx -C $AZKABAN_HOME
+pv $HOME_DIR/softwares/azkaban/azkaban-exec-server-3.84.4.tar.gz | tar -zx -C $AZKABAN_HOME
+pv $HOME_DIR/softwares/azkaban/azkaban-web-server-3.84.4.tar.gz | tar -zx -C $AZKABAN_HOME
+mv $AZKABAN_HOME/azkaban-exec-server-3.84.4 $AZKABAN_HOME/azkaban-exec
+mv $AZKABAN_HOME/azkaban-web-server-3.84.4 $AZKABAN_HOME/azkaban-web
+
+#############################################################################################
+# configure environment variables
+#############################################################################################
+printf -- "\n"
+printf -- "${INFO}>>> Configure azkaban environment variables.${END}\n"
+
+if [ $(grep -c "AZKABAN_HOME" $MARMOT_PROFILE) -eq '0' ]; then
+    echo -e >>$MARMOT_PROFILE
+    echo '#***** AZKABAN_HOME *****' >>$MARMOT_PROFILE
+    echo "export AZKABAN_HOME="$AZKABAN_HOME >>$MARMOT_PROFILE
+
+    source /etc/profile
+    printf -- "${SUCCESS}AZKABAN_HOME configure successful: $AZKABAN_HOME${END}\n"
+else
+    source /etc/profile
+    printf -- "${WARN}AZKABAN_HOME configurtion is complete.${END}\n"
+fi
+
+#############################################################################################
+# configure azkaban database
+#############################################################################################
+printf -- "\n"
+printf -- "${INFO}>>> Configure azkaban database.${END}\n"
+
 mysql -uroot -p$MYSQL_ROOT_PASS -e "use azkaban"
 
 if [[ $? -ne 0 ]]; then
-    log_info "创建元数据库 azkaban"
+    printf -- "${INFO}--> Create database azkaban.${END}\n"
     mysql -uroot -p$MYSQL_ROOT_PASS -e "CREATE DATABASE azkaban DEFAULT CHARSET utf8 COLLATE utf8_general_ci"
 
     mysql -uroot -p$MYSQL_ROOT_PASS -e "CREATE USER '$MYSQL_AZKABAN_USER'@'%' IDENTIFIED BY '$MYSQL_AZKABAN_PASS'"
@@ -75,13 +78,17 @@ if [[ $? -ne 0 ]]; then
         echo "max_allowed_packet=1024M" >>/etc/my.cnf
         systemctl restart mysqld
     fi
+    printf -- "${SUCCESS}Configure azkaban database successful.${END}\n"
 else
-    log_warn "数据库 azkaban 已创建"
+    printf -- "${SUCCESS}Database azkaban is complete.${END}\n"
 fi
 
-#################################
-# 配置 executor server
-#################################
+#############################################################################################
+# configure executor server
+#############################################################################################
+printf -- "\n"
+printf -- "${INFO}>>> Configure azkaban executor server.${END}\n"
+
 EXECUTOER_CONF_FILE=$AZKABAN_HOME/azkaban-exec/conf/azkaban.properties
 if [ $(grep -c "America/Los_Angeles" $EXECUTOER_CONF_FILE) -ne '0' ]; then
     sed -i -r '/^default\.timezone\.id/s/.*/default\.timezone\.id=Asia\/Shanghai/' $EXECUTOER_CONF_FILE
@@ -89,31 +96,58 @@ if [ $(grep -c "America/Los_Angeles" $EXECUTOER_CONF_FILE) -ne '0' ]; then
     sed -i -r '/^mysql\.host/s/.*/mysql\.host='${azkaban_nodes[0]}'/' $EXECUTOER_CONF_FILE
     sed -i -r '/^mysql\.password/s/.*/mysql\.password='$MYSQL_AZKABAN_PASS'/' $EXECUTOER_CONF_FILE
     echo "executor.port=12321" >>$EXECUTOER_CONF_FILE
+
+    printf -- "${SUCCESS}Configure azkaban executor server successful.${END}\n"
+else
+    printf -- "${SUCCESS}Azkaban executor server configurtion is complete.${END}\n"
 fi
 
-#################################
-# 配置 web server
-#################################
+#############################################################################################
+# configure azkaban web server
+#############################################################################################
+printf -- "\n"
+printf -- "${INFO}>>> Configure azkaban web server.${END}\n"
+
 WEB_CONF_FILE=$AZKABAN_HOME/azkaban-web/conf/azkaban.properties
 if [ $(grep -c "America/Los_Angeles" $WEB_CONF_FILE) -ne '0' ]; then
     sed -i -r '/^default\.timezone\.id/s/.*/default\.timezone\.id=Asia\/Shanghai/' $WEB_CONF_FILE
     sed -i -r '/^azkaban\.executorselector\.filters/s/.*/azkaban\.executorselector\.filters=StaticRemainingFlowSize,CpuStatus/' $WEB_CONF_FILE
     sed -i -r '/^mysql\.host/s/.*/mysql\.host='${azkaban_nodes[0]}'/' $WEB_CONF_FILE
     sed -i -r '/^mysql\.password/s/.*/mysql\.password='$MYSQL_AZKABAN_PASS'/' $WEB_CONF_FILE
+    printf -- "${SUCCESS}Configure azkaban web server successful.${END}\n"
+else
+    printf -- "${SUCCESS}Azkaban web server configurtion is complete.${END}\n"
 fi
+
+#############################################################################################
+# configure azkaban user
+#############################################################################################
+printf -- "\n"
+printf -- "${INFO}>>> Configure azkaban user.${END}\n"
 
 USER_CONF_FILE=$AZKABAN_HOME/azkaban-web/conf/azkaban-users.xml
-if [ $(grep -c "marmot" $WEB_CONF_FILE) -eq '0' ]; then
+if [ $(grep -c $AZKABAN_USER $WEB_CONF_FILE) -eq '0' ]; then
     USER_CONFIG='<user password="'$AZKABAN_PASS'" roles="admin" username="'$AZKABAN_USER'"/>'
     sed -in '/<\/azkaban-users>/i\'"$USER_CONFIG" $USER_CONF_FILE
+    printf -- "${SUCCESS}Configure azkaban user successful.${END}\n"
+else
+    printf -- "${SUCCESS}Azkaban user configurtion is complete.${END}\n"
 fi
 
 
-#################################
-# 同步分发集群
-#################################
+#############################################################################################
+# distributing azkaban
+#############################################################################################
+printf -- "\n"
+printf -- "${INFO}>>> Distributing azkaban to all cluster nodes.${END}\n"
 
-# 集群分发 azkaban-exec
-sh $SCRIPT_DIR/msync.sh $AZKABAN_NODES $AZKABAN_HOME/azkaban-exec
-# 集群分发环境变量
-sh $SCRIPT_DIR/msync.sh $AZKABAN_NODES /etc/profile.d/marmot_env.sh
+# modify permissions
+chown $HADOOP_USER:$HADOOP_USER -R $AZKABAN_HOME
+# distributing azkaban-exec
+sh $SCRIPT_DIR/msync $AZKABAN_NODES $AZKABAN_HOME/azkaban-exec
+printf -- "\n"
+# distributing environment variables
+sh $SCRIPT_DIR/msync $AZKABAN_NODES /etc/profile.d/marmot_env.sh
+
+printf -- "\n"
+printf -- "${SUCCESS}========== AZKABAN INSTALL SUCCESSFUL ==========${END}\n"
