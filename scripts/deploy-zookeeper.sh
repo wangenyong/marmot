c291b1574e5dac398707a45f797cd199b2ffd5a8
wangenyong <wangenyong@gamil.com>
2023-02-12 21:11:20 +0800

    submit

--- scripts/deploy-zookeeper.sh
+++ scripts/deploy-zookeeper.sh
@@ -1,62 +1,63 @@
 #!/bin/bash
 
-#################################
+#############################################################################################
 #
 # zookeeper version "3.5.7"
 #
-# 配置 zookeeper
+# configure zookeeper
 #
-#################################
+#############################################################################################
 
-# 获取当前脚本所在目录和项目根目录
+# get script directory and home directory
 SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 HOME_DIR="$(dirname $SCRIPT_DIR)"
-
-# 加载日志打印脚本
-source $SCRIPT_DIR/log.sh
-# 加载配置文件
+# loading config file
 source $HOME_DIR/conf/config.conf
-
+# loading printf file
+source $HOME_DIR/conf/printf.conf
+# loading zookeeper nodes
 IFS=',' read -ra zookeeper_nodes <<<$ZOOKEEPER_NODES
 
-log_info "========== 开始安装 ZOOKEEPER =========="
-
-# 判断 zookeeper 是否已经安装
-if [ -d /opt/marmot/apache-zookeeper-* ]; then
-    log_warn "zookeeper 已经安装!"
-else
-    # 安装 zookeeper
-    pv $HOME_DIR/softwares/apache-zookeeper-3.5.7-bin.tar.gz | tar -zx -C /opt/marmot/
-
-    # 创建环境变量文件
-    MARMOT_PROFILE="/etc/profile.d/marmot_env.sh"
-    if [ ! -f $MARMOT_PROFILE ]; then
-        touch $MARMOT_PROFILE
-    fi
-
-    # 配置 zookeeper 环境变量
-    if [ $(grep -c "ZOOKEEPER_HOME" $MARMOT_PROFILE) -eq '0' ]; then
-        cd /opt/marmot/apache-zookeeper-*
-        ZOOKEEPER_HOME="ZOOKEEPER_HOME="$(pwd)
-        cd -
-
-        echo -e >>$MARMOT_PROFILE
-        echo '#***** ZOOKEEPER_HOME *****' >>$MARMOT_PROFILE
-        echo "export "$ZOOKEEPER_HOME >>$MARMOT_PROFILE
-
-        source /etc/profile
-
-        log_info "ZOOKEEPER_HOME 环境变量设置完成: "$ZOOKEEPER_HOME
-    else
-        log_warn "ZOOKEEPER_HOME 环境变量已配置"
-    fi
-    log_info "========== ZOOKEEPER 安装完成 =========="
+printf -- "${INFO}========== INSTALL ZOOKEEPER ==========${END}\n"
+if [ -d $PROJECT_DIR/apache-zookeeper* ]; then
+    printf -- "${SUCCESS}========== ZOOKEEPER INSTALLED ==========${END}\n"
+    printf -- "\n"
+    exit 0
 fi
 
-#################################
-# 配置集群节点
-#################################
-source /etc/profile
+#############################################################################################
+# install zookeeper
+#############################################################################################
+printf -- "${INFO}>>> Install zookeeper.${END}\n"
+pv $HOME_DIR/softwares/apache-zookeeper-3.5.7-bin.tar.gz | tar -zx -C $PROJECT_DIR/
+
+#############################################################################################
+# configure environment variables
+#############################################################################################
+printf -- "\n"
+printf -- "${INFO}>>> Configure zookeeper environment variables.${END}\n"
+
+if [ $(grep -c "ZOOKEEPER_HOME" $MARMOT_PROFILE) -eq '0' ]; then
+    cd /opt/marmot/apache-zookeeper*
+    ZOOKEEPER_HOME="ZOOKEEPER_HOME="$(pwd)
+    cd - >/dev/null 2>&1
+
+    echo -e >>$MARMOT_PROFILE
+    echo '#***** ZOOKEEPER_HOME *****' >>$MARMOT_PROFILE
+    echo "export "$ZOOKEEPER_HOME >>$MARMOT_PROFILE
+
+    source /etc/profile
+    printf -- "${SUCCESS}ZOOKEEPER_HOME configure successful: $ZOOKEEPER_HOME${END}\n"
+else
+    source /etc/profile
+    printf -- "${WARN}ZOOKEEPER_HOME configurtion is complete.${END}\n"
+fi
+
+#############################################################################################
+# configure zookeeper cluster nodes
+#############################################################################################
+printf -- "\n"
+printf -- "${INFO}>>> Configure zookeeper cluster nodes.${END}\n"
 
 if [ ! -d "$ZOOKEEPER_HOME/data" ]; then
     mkdir -p $ZOOKEEPER_HOME/data
@@ -67,13 +68,13 @@ if [ ! -d "$ZOOKEEPER_HOME/data" ]; then
     sed -i -r '/^dataDir/s|.*|dataDir='$ZOOKEEPER_HOME'/data|' $ZOOKEEPER_HOME/conf/zoo.cfg
     echo "########## cluster ###########" >>$ZOOKEEPER_HOME/conf/zoo.cfg
 
-    # 修改 zookeeper 项目权限为 marmot:marmot
-    chown marmot:marmot -R $ZOOKEEPER_HOME
+    # modify permissions
+    chown $HADOOP_USER:$HADOOP_USER -R $ZOOKEEPER_HOME
 
-    # 集群分发 zookeeper
-    sh $SCRIPT_DIR/msync.sh $ZOOKEEPER_NODES $ZOOKEEPER_HOME
-    # 集群分发环境变量
-    sh $SCRIPT_DIR/msync.sh $ZOOKEEPER_NODES /etc/profile.d/marmot_env.sh
+    # distributing zookeeper
+    sh $SCRIPT_DIR/msync $ZOOKEEPER_NODES $ZOOKEEPER_HOME
+    # distributing environment variables
+    sh $SCRIPT_DIR/msync $ZOOKEEPER_NODES /etc/profile.d/marmot_env.sh
 
     i=1
     for node in ${zookeeper_nodes[@]}; do
@@ -81,7 +82,12 @@ if [ ! -d "$ZOOKEEPER_HOME/data" ]; then
         ssh $HADOOP_USER@$node "echo $i >>$ZOOKEEPER_HOME/data/myid"
         let i+=1
     done
-    # 集群分发配置文件
-    sh $SCRIPT_DIR/msync.sh $ZOOKEEPER_NODES $ZOOKEEPER_HOME/conf/zoo.cfg
+    # distributing zookeeper config file
+    sh $SCRIPT_DIR/msync $ZOOKEEPER_NODES $ZOOKEEPER_HOME/conf/zoo.cfg
+    printf -- "${SUCCESS}Configure zookeeper successful.${END}\n"
+else
+    printf -- "${SUCCESS}Zookeeper configurtion is complete.${END}\n"
 fi
 
+printf -- "\n"
+printf -- "${SUCCESS}========== ZOOKEEPER INSTALL SUCCESSFUL ==========${END}\n"
