77ae73fd856d500d3149007361e3793a8ffe9e42
wangenyong <wangenyong@gamil.com>
2023-03-12 21:33:11 +0800

    update deploy flink

--- scripts/deploy-flink.sh
+++ scripts/deploy-flink.sh
@@ -15,7 +15,8 @@ HOME_DIR="$(dirname $SCRIPT_DIR)"
 source $HOME_DIR/conf/config.conf
 # loading printf file
 source $HOME_DIR/conf/printf.conf
-# loading cluster nodes
+# loading version file
+source $HOME_DIR/conf/version.conf
 
 printf -- "${INFO}========== INSTALL FLINK ==========${END}\n"
 if [ -d $PROJECT_DIR/flink* ]; then
@@ -28,7 +29,7 @@ fi
 # install flink
 #############################################################################################
 printf -- "${INFO}>>> Install flink.${END}\n"
-pv $HOME_DIR/softwares/flink-1.13.0-bin-scala_2.12.tgz | tar -zx -C $PROJECT_DIR/
+pv $HOME_DIR/softwares/hadoop/flink-${flink_version}-*.tgz | tar -zx -C $PROJECT_DIR/
 
 #############################################################################################
 # configure environment variables
@@ -37,13 +38,12 @@ printf -- "\n"
 printf -- "${INFO}>>> Configure flink environment variables.${END}\n"
 
 if [ $(grep -c "FLINK_HOME" $MARMOT_PROFILE) -eq '0' ]; then
-    cd $PROJECT_DIR/flink*
-    FLINK_PATH="FLINK_HOME="$(pwd)
-    cd - >/dev/null 2>&1
+    FLINK_PATH="FLINK_HOME="$PROJECT_DIR/flink-${flink_version}
 
     echo -e >>$MARMOT_PROFILE
     echo '#***** FLINK_HOME *****' >>$MARMOT_PROFILE
     echo "export "$FLINK_PATH >>$MARMOT_PROFILE
+    echo 'export PATH=$PATH:$FLINK_HOME/bin' >>$MARMOT_PROFILE
 
     source /etc/profile
     printf -- "${SUCCESS}FLINK_HOME configure successful: $FLINK_HOME${END}\n"
